28d8d2e8d740f03de90dc5b20e8d24b9d19821ab
abhikumar12 <abhimanyukumar38@gmail.com>
2026-01-04 20:17:47 +0100

    Create sever.js

---
+++ app/sever.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const requestHandler = (request, response) => {
+  console.log(Received request for URL: ${request.url});
+
+  if (request.url === '/health') {
+    response.statusCode = 200;
+    response.end('OK');
+  } else {
+    response.statusCode = 200;
+    response.end('Hello World!');
+  }
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('Something went wrong', err);
+  }
+
+  console.log(Server is listening on ${port});
+});
